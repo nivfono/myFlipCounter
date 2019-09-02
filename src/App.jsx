006f0706a13f0323df96f598a2957f4251dbee55
nivfono <nivfono003@gmail.com>
2019-09-02 17:06:10 +0300

    changing the add function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Counter from "/home/trucknet/myreact/src/FlipCounter.jsx";
-
+import NCounter from "./NewFlipCounter"
 import "./App.css";
 class App extends Component {
   render() {
     return (
       <div id="body">
         <Counter />
+<hr></hr>
+    
+    
+     <NCounter />
       </div>
     );
   }
--- src/FlipCounter.jsx
+++ src/FlipCounter.jsx
@@ -4,62 +4,20 @@ class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count0: 0,
-      count1: 0,
-      count2: 0,
-      count3: 0,
-      count4: 0,
-      count5: 0,
-      count6: 0
+      myArray: ["0", "0", "0", "0", "0", "0"],
+      x: 0
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   add = () => {
-    if (this.state.count0 == 9) {
-      this.setState({ count0: (this.state.count0 = -1) });
-      this.setState({ count1: this.state.count1 + 1 });
-    }
-
-    if (this.state.count1 == 9) {
-      this.setState({ count1: (this.state.count1 = -1) });
-      this.setState({ count2: this.state.count2 + 1 });
-    }
-
-    if (this.state.count2 == 9) {
-      this.setState({ count2: (this.state.count2 = -1) });
-      this.setState({ count3: this.state.count3 + 1 });
-    }
-
-    if (this.state.count3 == 9) {
-      this.setState({ count3: (this.state.count3 = -1) });
-      this.setState({ count4: this.state.count4 + 1 });
-    }
-
-    if (this.state.count4 == 9) {
-      this.setState({ count4: (this.state.count4 = -1) });
-      this.setState({ count5: this.state.count5 + 1 });
-    }
-
-    this.setState({
-      count0: this.state.count0 + 1
-    });
+    this.setState({ x: this.state.x + 1 });
   };
+  handleSubmit=()=>{
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
 
-  handleSubmit(event) {
-    this.setState({ count0: parseInt(this.state.value % 10) });
-    this.setState({ count1: parseInt((this.state.value / 10) % 10) });
-    this.setState({ count2: parseInt((this.state.value / 100) % 10) });
-    this.setState({ count3: parseInt((this.state.value / 1000) % 10) });
-    this.setState({ count4: parseInt((this.state.value / 10000) % 10) });
-    this.setState({ count5: parseInt((this.state.value / 100000) % 10) });
-    event.preventDefault();
-  }
+  };
+ 
+
 
   render() {
     return (
@@ -68,12 +26,12 @@ class Counter extends Component {
         <h1>
           <table>
             <tr>
-              <td id="main">{this.state.count5}</td>
-              <td id="main">{this.state.count4}</td>
-              <td id="main">{this.state.count3}</td>
-              <td id="main">{this.state.count2}</td>
-              <td id="main">{this.state.count1}</td>
-              <td id="main">{this.state.count0}</td>
+              <td id="main">{parseInt((this.state.x / 100000) % 10)}</td>
+              <td id="main">{parseInt((this.state.x / 10000) % 10)}</td>
+              <td id="main">{parseInt((this.state.x / 1000) % 10)}</td>
+              <td id="main">{parseInt((this.state.x / 100) % 10)}</td>
+              <td id="main">{parseInt(this.state.x / 10) % 10}</td>
+              <td id="main">{parseInt(this.state.x % 10)}</td>
             </tr>
           </table>
         </h1>
