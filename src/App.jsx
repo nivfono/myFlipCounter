6742d66372f776be7e1d11547fe77b8025f21e5f
nivfono <nivfono003@gmail.com>
2019-09-02 15:11:57 +0300

    changing to jsx file

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import Counter from "/home/trucknet/myreact/src/FlipCounter.jsx";
+
+import "./App.css";
+class App extends Component {
+  
+
+render() {
+    return (
+ 
+ <div id="body">
+<Counter />
+<h1>-----------------------------------------------</h1>
+<ul id="links">
+<a href="#p">about</a>
+<a href="https://www.trucknet.io/he">the solution</a>
+<a href="https://www.trucknet.io/he">partners</a>
+<a href="https://www.trucknet.io/he">sign in</a>
+<a href="https://www.trucknet.io/he">news</a>
+<a href="https://www.trucknet.io/he">contact us</a>
+<a href="https://www.trucknet.io/he">enter</a>
+
+</ul>
+
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+  <p id="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+
+
+</div>  
+        
+
+    );
+  }
+}
+
+export default App;
