50b2feb253df15fbc0e49010f39774f230b918df
nivfono <nivfono003@gmail.com>
2019-09-03 14:04:19 +0300

    remove console logs

--- src/FlipCounter.jsx
+++ src/FlipCounter.jsx
@@ -6,32 +6,24 @@ class Counter extends Component {
     super(props);
     this.state = {
       x: 0
-  
     };
   }
-  add = (num) => {
+  add = num => {
     this.setState({ x: parseInt(this.state.x) + 1 });
-    console.log("add activated");
-    console.log(this.state.x);
-  };
- 
- 
- 
-  myChangeHandler = (event) => {
-    this.setState({ value: event.target.value });
-    console.log("change activated");
   };
 
-  mySubmitHandler = (event) => {
+  myChangeHandler = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  mySubmitHandler = event => {
     this.setState({ x: this.state.value });
-    console.log("submit activated");
+
     event.preventDefault();
   };
 
   render() {
-   console.log(this.state.x)
-   console.log(this.state.value)
-   const arr = [
+    const arr = [
       parseInt((this.state.x / 100000) % 10),
       parseInt((this.state.x / 10000) % 10),
       parseInt((this.state.x / 1000) % 10),
@@ -40,21 +32,21 @@ class Counter extends Component {
       parseInt(this.state.x % 10)
     ];
 
-
-    
     return (
       <div>
         <p>this is my counter: </p>
         <h1>
           <table>
-            <tr>
-              <td id="main">{arr[0]}</td>
-              <td id="main">{arr[1]}</td>
-              <td id="main">{arr[2]}</td>
-              <td id="main">{arr[3]}</td>
-              <td id="main">{arr[4]}</td>
-              <td id="main">{arr[5]}</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td id="main">{arr[0]}</td>
+                <td id="main">{arr[1]}</td>
+                <td id="main">{arr[2]}</td>
+                <td id="main">{arr[3]}</td>
+                <td id="main">{arr[4]}</td>
+                <td id="main">{arr[5]}</td>
+              </tr>
+            </tbody>
           </table>
         </h1>
         <button onClick={this.add}>add</button>
