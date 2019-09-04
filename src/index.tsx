d9074f33ecf0a63bb18efa6c3318e81eee03232c
nivfono <nivfono003@gmail.com>
2019-09-04 11:23:12 +0300

    connecting all files to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "/home/trucknet/myreact/src/App.tsx";
+import App from "/home/trucknet/myreact/src/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
