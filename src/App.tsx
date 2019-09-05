8d5a8ddc15a20f272acae8a6f8846c4eefd8b9fb
nivfono <nivfono003@gmail.com>
2019-09-05 12:50:53 +0300

    component receives one prop value with the type Number

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
-import Counter from "/home/niv/myflipcounter/myflipcounter/src/FlipCounter.tsx";
+import FlipCounter from "./FlipCounter";
 
 import "./App.css";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 0
+    };
+  }
+  add = () => {
+    this.setState({ x: parseInt(this.state.x) + 1 });
+  };
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  handleSubmit = event => {
+    this.setState({ x: this.state.value });
+
+    event.preventDefault();
+  };
+
   render() {
+    let x = this.state.x;
+    console.log(x);
     return (
       <div id="body">
-        <Counter />
+        <FlipCounter title={this.state.x} />
+        <button onClick={this.add}>add</button>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
--- src/FlipCounter.tsx
+++ src/FlipCounter.tsx
@@ -1,66 +1,33 @@
 import React, { Component } from "react";
 import "./FlipCounter.css";
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      x: 0
-    };
+const FlipCounter = props => {
+  var mystring = String(props.title);
+  var arr = mystring.split("");
+  arr.reverse();
+
+  if (arr.length < 6) {
+    for (var i = 0; i < 6 - (arr.length - 4); i++) arr.push("0");
   }
-  add = () => {
-    this.setState({ x: parseInt(this.state.x) + 1 });
-  };
+  return (
+    <div>
+      <p>mystring</p>
+      <h1>
+        <table>
+          <tbody>
+            <tr>
+              <td id="main">{arr[5]}</td>
+              <td id="main">{arr[4]}</td>
+              <td id="main">{arr[3]}</td>
+              <td id="main">{arr[2]}</td>
+              <td id="main">{arr[1]}</td>
+              <td id="main">{arr[0]}</td>
+            </tr>
+          </tbody>
+        </table>
+      </h1>
+    </div>
+  );
+};
 
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
-
-  handleSubmit = event => {
-    this.setState({ x: this.state.value });
-
-    event.preventDefault();
-  };
-
-  render() {
-    let mystring = String(this.state.x);
-
-    var arr = mystring.split("");
-    arr.reverse();
-
-    if (arr.length < 6) {
-      for (var i = 0; i < 6 - (arr.length - 4); i++) arr.push("0");
-    }
-
-    return (
-      <div>
-        <p>mystring</p>
-        <h1>
-          <table>
-            <tbody>
-              <tr>
-                <td id="main">{arr[5]}</td>
-                <td id="main">{arr[4]}</td>
-                <td id="main">{arr[3]}</td>
-                <td id="main">{arr[2]}</td>
-                <td id="main">{arr[1]}</td>
-                <td id="main">{arr[0]}</td>
-              </tr>
-            </tbody>
-          </table>
-        </h1>
-        <button onClick={this.add}>add</button>
-
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
-  }
-}
-export default Counter;
+export default FlipCounter;
